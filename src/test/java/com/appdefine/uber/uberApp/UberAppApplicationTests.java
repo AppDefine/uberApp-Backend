381a5f38cf911820a094d188fe6ca440f77f8717
kaunik <appdefine@gmail.com>
2025-10-28 20:18:36 +0530

    [+] Added Emails Send Support

--- src/test/java/com/appdefine/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/appdefine/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,35 @@
 package com.appdefine.uber.uberApp;
 
+import com.appdefine.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+		emailSenderService.sendEmail(
+				"gimok31307@ametitas.com",
+				"Subject Email",
+				"Body Email");
+	}
+
+	@Test
+	void sendEmailMultiple(){
+		String email[] = {
+				"gimok31307@ametitas.com",
+				"flexuswriter@gmail.com"
+		};
+
+		emailSenderService.sendEmail(
+				email,
+				"Subject Email 22",
+				"Body Email 22");
 	}
 
 }
